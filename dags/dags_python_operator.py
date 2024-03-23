5abc245ace0841e5372477b235e12c1428837bc2
woody <woody@woodyui-MacBookAir.local>
2024-03-23 11:04:37 +0900

    Python Operator 예제 추가

---
+++ dags/dags_python_operator.py
@@ -0,0 +1,23 @@
+from airflow import DAG
+import datetime
+import pendulum
+from airflow.operators.python import PythonOperator
+import random
+
+with DAG(
+    dag_id="dags_python_operator",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False,
+) as dag:
+
+    def select_fruit():
+        fruit = ["APPLE", "BANANA", "ORANGE", "AVOCADO"]
+        rand_int = random.randint(0, 3)
+        print(fruit[rand_int])
+
+    py_t1 = PythonOperator(
+        task_id="pyt1",
+        python_callable=select_fruit,  # 실행할 함수 입력
+    )
+    py_t1
