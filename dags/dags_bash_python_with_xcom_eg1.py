fa9feb5ec07169cf7c6c660c52f2e2bc7741dd82
woody <woody@woodyui-MacBookAir.local>
2024-03-27 22:12:38 +0900

    python & bash 오퍼레이터 xcom 사용 예시 추가

---
+++ dags/dags_bash_python_with_xcom_eg1.py
@@ -0,0 +1,28 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.decorators import task
+import datetime
+import pendulum
+
+with DAG(
+    dag_id="dags_bash_python_with_xcom_eg1",
+    schedule="10 0 * * *",
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False,
+) as dag:
+    bash_push = BashOperator(
+        task_id="bash_push",
+        bash_command="echo PUSH_START "
+        '{{ti.xcom_push(key="bash_pushed", value=200)}} && '
+        "echo PUSH_COMPLETE",
+    )
+
+    @task(task_id="python_pull")
+    def python_pull_xcom(**kwargs):
+        ti = kwargs["ti"]
+        status_value = ti.xcom_pull(key="bash_pushed")
+        return_value = ti.xcom_pull(task_ids="bash_push")
+        print("status_value:" + str(status_value))
+        print("return_value:" + return_value)
+
+    bash_push >> python_pull_xcom()
